ff3bad7890d4ba2d1521040b9f88ff1763fecf69
euisang0212 <81742091+euisang0212@users.noreply.github.com>
2025-01-06 19:48:39 +0900

    [level 2] Title: H-Index, Time: 0.11 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -0,0 +1,13 @@
+def solution(citations):
+    answer = 0
+
+    citations.sort()
+    n = len(citations)
+
+    for i in range(n):
+        hIndex = n-i
+        if citations[i] >= hIndex:
+            answer = hIndex
+            break
+    
+    return answer
