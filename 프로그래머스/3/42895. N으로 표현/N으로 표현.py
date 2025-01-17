59c79cfd1d77ebd6fae04aa17c2b05fc74d35eee
euisang0212 <81742091+euisang0212@users.noreply.github.com>
2025-01-17 13:15:01 +0900

    [level 3] Title: N으로 표현, Time: 10.40 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.py
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.py
@@ -2,24 +2,25 @@ def solution(N, number):
     if N == number:
         return 1
 
-    # DP 테이블 초기화
-    dp = [set() for _ in range(9)]  # dp[i]: N을 i번 사용해서 만들 수 있는 숫자의 집합
+    # DP 테이블 초기화 (1 ~ 8 사용 가능)
+    dp = [set() for _ in range(9)]
     for i in range(1, 9):
-        dp[i].add(int(str(N) * i))  # N을 i번 연속 사용한 숫자 추가 (ex: 5, 55, 555)
+        dp[i].add(int(str(N) * i))  # N을 i번 연속 사용한 숫자 추가
 
-    # 동적 프로그래밍 진행
-    for i in range(1, 9):
-        for j in range(1, i):
+    # 동적 프로그래밍
+    for i in range(1, 9):  # N 사용 횟수
+        for j in range(1, i):  # i를 두 부분으로 나누어 조합
             for x in dp[j]:
                 for y in dp[i - j]:
-                    dp[i].add(x + y)
-                    dp[i].add(x - y)
-                    dp[i].add(x * y)
-                    if y != 0:  # 0으로 나누는 경우 제외
+                    dp[i].add(x + y)  # 덧셈
+                    if x - y > 0:  # 음수와 0 제외
+                        dp[i].add(x - y)
+                    dp[i].add(x * y)  # 곱셈
+                    if y != 0:  # 나눗셈에서 분모가 0이 아닌 경우
                         dp[i].add(x // y)
-        # 목표 숫자가 현재 집합에 있다면 반환
+
+        # 목표 숫자 확인
         if number in dp[i]:
             return i
 
-    # 8번 이상 사용해도 목표 숫자를 못 만들면 -1 반환
-    return -1
+    return -1  # 8번 이상 사용해도 목표 숫자를 만들 수 없으면 -1
