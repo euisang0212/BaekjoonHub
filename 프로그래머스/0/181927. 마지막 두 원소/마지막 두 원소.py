5a007b37622c1058240b7ef663c2c7cfd49a3a06
euisang0212 <81742091+euisang0212@users.noreply.github.com>
2025-02-24 16:15:46 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
@@ -0,0 +1,13 @@
+def solution(num_list):
+    answer = []
+    
+    n = len(num_list)
+    
+    if num_list[n-1] > num_list[n-2]:
+        num_list.append(num_list[n-1] - num_list[n-2])
+    else:
+        num_list.append(2 * num_list[n-1])
+    
+    answer = num_list
+    
+    return answer
